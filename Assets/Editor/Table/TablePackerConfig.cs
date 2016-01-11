a2c6890ba2617a07d5cd60d570dd8aded5812527
laijingfeng <337292616@qq.com>
2016-01-11 13:44:43 +0800

    更新配置

--- Assets/Editor/Table/TablePackerConfig.cs
+++ Assets/Editor/Table/TablePackerConfig.cs
@@ -11,4 +11,9 @@ public partial class TablePacker : EditorWindow
     /// 表格相关内容所在目录
     /// </summary>
     private static string m_strTablePath = Application.dataPath + "/../table/";
+
+    /// <summary>
+    /// table所在文件夹名称
+    /// </summary>
+    private static string m_strTableFileName = "table";
 }
